f6e49cd08afe479493071023c87a42cdff057d2c
Todd Hay <todd.hay@noaa.gov>
2019-04-17 19:58:00 -0700

    Initial fluorescence equation info

--- src/sbe19plusV2/equations/fluorescence.ts
+++ src/sbe19plusV2/equations/fluorescence.ts
@@ -4,9 +4,22 @@ import { col } from 'apache-arrow/compute/predicate';
 import * as assert from 'assert';
 
 
-export function fluorescence() {
+export function fluorescence(df: Table): Table {
+    /*
+        Fluorescence reference:  SBE 19plusV2 manual, p. 57, Fluormeter Calibration Coefficients
 
 
+    */
+    let Cfn: number = null, A1: number = null, A2: number = null, B: number = null;
+    let Fn: number = null, prod: number = null, Chl: number = null;
+
+    let V: number = null, PAR: number = null;
+
+    Fn = Cfn * (10 ^ V);
+    prod = A1 * Fn / (A2 + PAR);
+    Chl = Fn / (B * PAR)
+
+    return df;
 }
 
 
